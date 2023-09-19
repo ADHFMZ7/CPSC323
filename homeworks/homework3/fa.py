c29c4bca58e701e78290482440853ea46a6fd799
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-09-19 04:59:06 -0700

    fixed finite automata

--- homeworks/homework3/fa.py
+++ homeworks/homework3/fa.py
@@ -10,11 +10,10 @@ def main():
 
     inp = sys.argv[1] 
     
-    
     fa = {
         0: {'a': 0, 'b': 1, 'c': 2},
         1: {'a': 2, 'b': 1, 'c': 4},
-        2: {'a': 1, 'b': 4, 'c': 4},
+        2: {'a': 0, 'b': 4, 'c': 4},
         'finish': (1, 2)
     }
 
@@ -25,11 +24,8 @@ def validate(input_string, fa):
     state = 0
 
     for symbol in input_string:
-        print(symbol, state)
         state = fa[state][symbol] 
-        if state == 4:
-            return False
-
+    
     return state in fa['finish']
 
 if __name__ == '__main__':
